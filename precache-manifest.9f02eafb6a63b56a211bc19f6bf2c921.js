55a02a529612c68cf392b115e067c025c197e2a0
phucthang1101 <phucthangvt1101@gmail.com>
2020-04-30 23:26:41 +0700

    Updates

---
+++ precache-manifest.9f02eafb6a63b56a211bc19f6bf2c921.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d3de93dae115e50607c2ae48e0bb7f9",
+    "url": "/resume_v2/index.html"
+  },
+  {
+    "revision": "7670a235a93d80c0a388",
+    "url": "/resume_v2/static/css/main.51ea8c6c.chunk.css"
+  },
+  {
+    "revision": "55bbb93e82211c529737",
+    "url": "/resume_v2/static/js/2.ec32db9a.chunk.js"
+  },
+  {
+    "revision": "b1c48fb50b52ff7e51ea57a4c76b798d",
+    "url": "/resume_v2/static/js/2.ec32db9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7670a235a93d80c0a388",
+    "url": "/resume_v2/static/js/main.83292255.chunk.js"
+  },
+  {
+    "revision": "9f03cae6ee8b77226c6d",
+    "url": "/resume_v2/static/js/runtime-main.d21a40b8.js"
+  },
+  {
+    "revision": "f81f938ac97ef25d1d596f43cf0c8484",
+    "url": "/resume_v2/static/media/cd-arrow.f81f938a.svg"
+  }
+]);
